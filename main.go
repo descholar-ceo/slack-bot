f4a41d309a7c6094560949f96c31dcec77a761f7
descholar-ceo <emmanuellamugi@gmail.com>
2020-08-19 16:46:03 +0200

    Message can be sent

--- main.go
+++ main.go
@@ -21,8 +21,8 @@ func main() {
 
 	slackClient = slack.New(
 		slackAccessToken,
-		slack.OptionDebug(true),
-		slack.OptionLog(log.New(os.Stdout, "slack bot:", log.Lshortfile|log.LstdFlags)),
+		// slack.OptionDebug(true),
+		// slack.OptionLog(log.New(os.Stdout, "slack bot:", log.Lshortfile|log.LstdFlags)),
 	)
 
 	rtm := slackClient.NewRTM()
@@ -37,5 +37,29 @@ func main() {
 }
 
 func handleMsgFromSlack(event *slack.MessageEvent) {
-	fmt.Printf("%v\n", event)
+	attachment := slack.Attachment{
+		Pretext: "Hello descholar",
+		Text:    "I am happy to see you here!",
+		// Uncomment the following part to send a field too
+
+		Fields: []slack.AttachmentField{
+			slack.AttachmentField{
+				Title: "a",
+				Value: "no",
+			},
+		},
+	}
+
+	// fmt.Printf("This is the event obj %v: \n", event.Username)
+	channelID, timestamp, err := slackClient.PostMessage(
+		event.User,
+		slack.MsgOptionText("Some text", false),
+		slack.MsgOptionAttachments(attachment),
+	)
+
+	if err != nil {
+		fmt.Printf("Ooops! There is an error: %s\n", err)
+		return
+	}
+	fmt.Printf("Message successfully sent to channel %s at %s", channelID, timestamp)
 }
