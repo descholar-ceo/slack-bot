3acb65206077eeab7d549e147301af61e2196940
descholar-ceo <emmanuellamugi@gmail.com>
2020-08-19 16:14:06 +0200

    Add more configs

---
+++ go.mod
@@ -0,0 +1,8 @@
+module go-env-ways
+
+go 1.15
+
+require (
+	github.com/joho/godotenv v1.3.0
+	github.com/slack-go/slack v0.6.6
+)
--- main.go
+++ main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/slack-go/slack"
+)
+
+var slackClient *slack.Client
 
 func main() {
-	fmt.Println("Hello there! This is the slack-bot")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	slackAccessToken := os.Getenv("SLACK_ACCESS_TOKEN")
+
+	slackClient = slack.New(
+		slackAccessToken,
+		slack.OptionDebug(true),
+		slack.OptionLog(log.New(os.Stdout, "slack bot:", log.Lshortfile|log.LstdFlags)),
+	)
+
+	rtm := slackClient.NewRTM()
+	go rtm.ManageConnection()
+
+	for msg := range rtm.IncomingEvents {
+		switch ev := msg.Data.(type) {
+		case *slack.MessageEvent:
+			go handleMsgFromSlack(ev)
+		}
+	}
+}
+
+func handleMsgFromSlack(event *slack.MessageEvent) {
+	fmt.Printf("%v\n", event)
 }
